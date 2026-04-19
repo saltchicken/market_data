a5e8e40f22cbedc3790a50425156cc9695feac92
saltchicken <john.eicher89@gmail.com>
2026-04-19 10:35:18 -0700

    Add strategy field

--- src/ibkr_alerts/sql/init_schema.sql
+++ src/ibkr_alerts/sql/init_schema.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS watchlist CASCADE;
 
 CREATE TABLE watchlist (
     ticker TEXT PRIMARY KEY,
+    strategy TEXT DEFAULT NULL,
     is_active BOOLEAN DEFAULT TRUE,
     target_buy DOUBLE PRECISION DEFAULT NULL,
     target_sell DOUBLE PRECISION DEFAULT NULL,
@@ -11,6 +12,7 @@ CREATE TABLE watchlist (
 );
 
 COMMENT ON TABLE watchlist IS 'Active symbols and alert thresholds monitored by IBKR.';
+COMMENT ON COLUMN watchlist.strategy IS 'Strategy that was used to identify the symbol.';
 COMMENT ON COLUMN watchlist.is_active IS 'Boolean flag to easily pause monitoring for a symbol without deleting it.';
 COMMENT ON COLUMN watchlist.target_buy IS 'Target limit price to trigger a buy alert.';
 COMMENT ON COLUMN watchlist.target_sell IS 'Target limit price to trigger a sell alert.';
