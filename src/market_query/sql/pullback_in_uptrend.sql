b268fe92b7f56dcb2d412784fc3b6ee482b87862
saltchicken <john.eicher89@gmail.com>
2026-04-19 17:26:22 -0700

    Add target_volume

--- src/market_query/sql/pullback_in_uptrend.sql
+++ src/market_query/sql/pullback_in_uptrend.sql
@@ -7,6 +7,12 @@ SELECT
   ROUND(CAST(close * 0.98 AS NUMERIC), 2) AS target_buy,
   ROUND(CAST(close * 1.05 AS NUMERIC), 2) AS target_sell,
 
+    -- TARGET VOLUME: 
+  -- 1. Take the 60-day average daily volume (vol_sma_60)
+  -- 2. Divide by 13 (number of 30-min periods in a trading day) to get average volume per 30-min bar
+  -- 3. Multiply by 2.0 to trigger an alert only when a 30-min bar sees 200% of average volume
+  ROUND(CAST((vol_sma_60 / 13.0) * 2.0 AS NUMERIC), 0) AS target_volume,
+
   sma_50_dist_pct,
   sma_200_dist_pct,
   ROUND(CAST(atr_14_pct AS NUMERIC), 2) AS atr_14_pct,
